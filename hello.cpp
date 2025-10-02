dc5b5f03bf71d59308023baf04049970e7b93cc5
Dmitriy Vorobyev <st140149@student.spbu.ru>
2025-10-03 00:15:44 +0300

    Update main.cpp, hello.cpp, hello.h with new functionality

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include "hello.h"
+
+#include <iostream>
+
+using namespace std;
+
+void sayHello(){
+    cout << "Hello, World!" << endl;
+
+    cout << "Write the name to continue or 'exit' to end the program" << endl;
+
+    string input;
+
+    cin >> input;
+
+    while (input != "exit"){
+        cout << "Hello, " << input << "!" << endl;
+
+        cout << "Write the name to continue or 'exit' to end the program" << endl;
+
+        cin >> input;
+    }
+}
